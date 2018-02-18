90a6a803b4afa8eae9024fd7c8cb13e191885914
Lina Khalil <g00059008@aus.edu>
2018-02-18 15:59:15 +0400

    Feature B added

--- COE420LA3/src/Lina.java
+++ COE420LA3/src/Lina.java
@@ -1,9 +1,13 @@
 
 public class Lina {
-
+void addfeatureB()
+{
+	System.out.println("lina is added ");
+}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello word");
+
 	}
 
 }
